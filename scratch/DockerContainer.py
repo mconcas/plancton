6dda0c7b0687408dd98b3d9ebf0520d199e18421
Matteo Concas <mett1990@gmail.com>
2015-07-23 16:43:13 +0200

    Initial Commit

---
+++ scratch/DockerContainer.py
@@ -0,0 +1,7 @@
+
+
+class DockerContainer:
+    """ Dumb class definition for Docker Containers """
+    def __init__(self, Id, status):
+        self.Id = Id
+        self.status = status
---
+++ scratch/__init__.py
@@ -0,0 +1,1 @@
+from .DockerContainer import DockerContainer
