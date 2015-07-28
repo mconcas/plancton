69f257eccc557d0850bcfe3875ae437a61bf42d9
Matteo Concas <mconcas@cern.ch>
2015-07-28 11:35:36 +0200

    Still W.I.P. refactoring and debugging.

--- devel/dev-container-spawner.py
+++ devel/dev-container-spawner.py
@@ -7,6 +7,7 @@ import json
 import time
 import logging, logging.handlers
 
+from stat import *
 from configobj import ConfigObj
 
 try:
@@ -17,7 +18,7 @@ except ImportError, e:
     + 'at least, in the working directory. '
     + str(e)
 
-    exit(-1)
+    os.exit(-1)
 
 class ContainerManager(object):
     __version__ = '0.0.1'
@@ -31,10 +32,10 @@ class ContainerManager(object):
         self._session = requests_unixsocket.Session()
         self._container_registry = []
 
-    ## Set up logs.
+    ## _setup_logger: sets up logs.
     #
     # @return nothing
-    def _setup_logfile(self):
+    def _setup_logger(self):
         format = '%(asctime)s %(name)s %(levelname)s ' + \
             '[%(module)s.%(funcName)s] %(message)s'
         datefmt = '%Y-%m-%d %H:%M:%S'
@@ -45,12 +46,12 @@ class ContainerManager(object):
         self._logger.setLevel(10)
         self._logger.addHandler(log_file_handler)
 
-    ## Set up from configfile.
+    ## _setup_from_conf: sets up from configfile.
     #
     # @return nothing
-    def _load_conf(self, file_path):
-        ## This function is called ONCE at the startup and load cinfigurations and
-        #  backups.
+    def _setup_from_conf(self, file_path):
+        ## This function is called ONCE at the startup, it loads configurations and
+        #  backup files.
 
         self._logger.info('Loading cfg from: ' + file_path)
         self._config = ConfigObj(file_path)
@@ -61,13 +62,122 @@ class ContainerManager(object):
         self._logpath = logpath
         self._socket_path = self._config[mancfg]['SocketPath']
         self._container_conf_file = self._config[mancfg]['ContainerConf']
-        self._container_registry_file = self._config[mancfg]['RegistryPath']
 
+        ## For now hardcoded, since it cannot be safely (re)created, preventing a wrong
+        #  behaviour for this script.
+        self._container_registry_file = '/tmp/dev-container-manager.reg'
 
+        # If any, recover from file.
+        self._update_registry_from_file()
 
+    ## _list_container: lists existing containers and their own statuses.
+    #
+    #  NEW:
+    #  Now it also performs an ID check with the internally stored list, substantially
+    #  making container-manager blind to "not owned" containers.
+    #  Thus is provided a sort of isolation.
+    #
+    #  @return the list length.
+    def _list_container(self):
+        """
+        This function wraps some requests, prints container list in the log file
+        and returns the number of running _owned_ containers.
+        """
+        try:
+            self._logger.debug('Trying to attach to : http+unix://'
+                + self._socket_path + '/containers/json?all=1')
+            self._logger.info('Checking container list...')
+            request = self._session.get('http+unix://' + self._socket_path
+                + '/containers/json?all=1')
+            request.raise_for_status()
+            self._jsoncontlist = json.loads(request.content)
+
+            contnum = len(self._jsoncontlist) # Total containers number.
+            self._logger.debug('Found %r container(s) in the whole Docker database.' \
+                % contnum)
+            self._logger.info('Listing below...')
+
+            ## Since the APIs return all the containers in the list, even the ones ran
+            #  by other users, we must provide, at some point, a kind of isolation.
+            #  Thus we can work safely using our local list of containers' Ids.
+            owned_containers = 0
+            for i in range(0, contnum):
+                status = ''
+                id = str(self._jsoncontlist[i]['Id'])
+                if id in self._container_registry:
+                    owner = 'spawner-agent'
+                    owned_containers+=1
+                    status = self._jsoncontlist[i]['Status']
+                    status if status else 'Created'
+                    self._logger.info('\t => ID: %s => OWNER: %s => STATUS: %s ' \
+                        % (id, owner, status))
+                else:
+                    owner = 'others'
+                    status = self._jsoncontlist[i]['Status']
+                    status if status else 'Created'
+                    self._logger.debug('\t => ID: %s => OWNER: %s => STATUS: %s ' \
+                        % (id, owner, status))
+
+            self._logger.info('Found %r "owned" container(s).' % owned_containers)
+
+            return owned_containers
+
+        except requests.exceptions.ConnectionError:
+            self._logger.debug('Connection Error: couldn\'t find a proper socket. '
+                + ' Is the Docker daemon running correctly?'
+                + ' Check if /var/run/docker.sock exists.' )
+
+            os.exit(-1)
+
+        except requests.exceptions.HTTPError as e:
+            print 'HTTPError raised, please check ' + self._logpath + \
+            ' for further informations.'
+            self._logger.debug('Bad answer from server. \n')
+            self._logger.error(e)
+
+            os.exit(-1)
+
+    ## _registry_file_update: performs an update of the backup registry file.
+    #
+    # @return nothing
+    def _registry_file_update(self):
+
+        flags = os.O_CREAT | os.O_EXCL | os.O_RDONLY
+        try:
+            file_handle = os.open(self._container_registry_file, flags, 0600)
+        except OSError as e:
+            if e.errno == errno.EEXIST:  # Failed as the file already exists.
+                self._logger.info('Found existent registry file: '
+                    + self._container_registry_file)
+
+                ## Checking right permissions.
+                reg_stats = os.stat(self._container_registry_file)
+                if not bool((reg_stats[ST_MODE] & S_IRWXG) or (reg_stats[ST_MODE] \
+                    & S_IRWXO)):
+                    with open(self._container_registry_file) as registry:
+
+                        ## Feed the registry.
+                        for line in self._container_registry:
+                            registry.write(line + '\n')
+                else:
+                    logging.error('Wrong permissions on %s, ignoring it.' \
+                        % self._container_registry_file)
+
+            else:  # Something unexpected went wrong so reraise the exception.
+                self._logger.debug('Failed to get a file descriptor for %s, '
+                    + ' nor existing .reg file. Traceback follows.' \
+                    % self._container_registry_file)
+                self._logger.error(e)
+
+                os.exit(-1)
+
+    ## _update_registry_from_file: performs an update from the backup registry file.
+    #
+    # @return nothing
+    def _update_registry_from_file(self):
         #### =============================================================================
         # Thanks to: stackoverflow.com/questions/10978869 for the advice.
-        # Trying to avoid race condition.
+        # Trying to avoid race condition, excluding symlinks.
         # When these two flags are specified, symbolic links are not followed: if pathname
         # is a symbolic link, then open() fails regardless of where the symbolic
         # link points to.
@@ -80,84 +190,33 @@ class ContainerManager(object):
 
         flags = os.O_CREAT | os.O_EXCL | os.O_RDONLY
         try:
-            file_handle = os.open(self._container_registry_file, flags)
+            file_handle = os.open(self._container_registry_file, flags, 0600)
         except OSError as e:
             if e.errno == errno.EEXIST:  # Failed as the file already exists.
-                self._logger.info('Found existent registry file: ' \
+                self._logger.info('Found existent registry file: '
                     + self._container_registry_file)
 
-                with open(self._container_registry_file) as registry:
-                    for line in registry:
-                        self._container_registry.append(line.rstrip('\n'))
+                ## Checking right permissions.
+                reg_stats = os.stat(self._container_registry_file)
+                if not bool((reg_stats[ST_MODE] & S_IRWXG) or (reg_stats[ST_MODE] \
+                    & S_IRWXO)):
+
+                    with open(self._container_registry_file) as registry:
+
+                        ## Feed the registry.
+                        for line in registry:
+                            self._container_registry.append(line.rstrip('\n'))
+                else:
+                    logging.error('Wrong permissions on %s, ignoring it.' \
+                        % self._container_registry_file)
+
             else:  # Something unexpected went wrong so reraise the exception.
-                self._logger.debug('Failed to get a file descriptor for %s, ' \
+                self._logger.debug('Failed to get a file descriptor for %s, '
                     + ' nor existing .reg file. Traceback follows.' \
                     % self._container_registry_file)
                 self._logger.error(e)
 
-                exit(-1)
-
-    ## Check for existing containers, independently from which are their statuses.
-    #
-    #  NEW:
-    #  Now it also performs an ID check with the internally stored list, substantially
-    #  making container-manager blind to "not owned" containers.
-    #  Thus is provided a sort of isolation.
-    #
-    # @return the list length.
-    def _list_container(self):
-        """
-        This function wraps some requests, print the container list in the log file
-        and return the number of running containers.
-        """
-        try:
-            self._logger.info('Trying to attach to : ' + 'http+unix://' \
-                + self._socket_path + '/containers/json?all=1')
-            request = self._session.get('http+unix://' + self._socket_path \
-                + '/containers/json?all=1')
-            request.raise_for_status()
-            self._logger.info('Checking container list...')
-            self._jsoncontlist = json.loads(request.content)
-            contnum = len(self._jsoncontlist)
-            self._logger.info('Found %r container(s) in the whole Docker database.' \
-                % contnum)
-
-            status = ''
-            owned_counter = 0
-            for i in range(0, contnum):
-                id = str(self._jsoncontlist[i]['Id'])
-                if id in self._container_registry:
-                    owner = 'spawner'
-                    owned_counter+=1
-                    status = self._jsoncontlist[i]['Status']
-                    status if status else 'Created'
-                    # self._logger.debug('Found container(s) of mine, listing...')
-                    self._logger.info('\t ~> ID: %s ~> STATUS: %s ~> OWNER: %s' \
-                        % (id, status, owner))
-                else:
-                    owner = 'others'
-                    status = self._jsoncontlist[i]['Status']
-                    status if status else 'Created'
-                    # self._logger.debug('Found container(s) not of mine, listing...')
-                    self._logger.debug('\t ~> ID: %s ~> STATUS: %s ~> OWNER: %s' \
-                        % (id, status, owner))
-
-            return owned_counter
-
-        except requests.exceptions.ConnectionError:
-            self._logger.debug('Conn. Error: couldn\'t find a proper socket. '
-                + ' Is the Docker daemon running correctly?'
-                + ' Check if /var/run/docker.sock exists.' )
-
-            exit(-1)
-
-        except requests.exceptions.HTTPError as e:
-            print 'HTTPError raised, please check ' + self._logpath + \
-            ' for further informations.'
-            self._logger.debug('Bad answer from server. \n')
-            self._logger.error(e)
-
-            exit(-1)
+                os.exit(-1)
 
     ## Create a container following from a json cfg file.
     #
@@ -185,8 +244,10 @@ class ContainerManager(object):
                     ## Before returning is a good choice to backup the registry to the
                     #  .reg file. Since a container creation is not done with an high
                     #  frequency this should not cause overhead issues.
-                    with open(self._container_registry_file, 'a') as registry:
-                        registry.write(c_id + '\n')
+
+                    # with open(self._container_registry_file, 'a') as registry:
+                    #    registry.write(c_id + '\n')
+                    self._registry_file_update()
 
                     return c_id
 
@@ -196,26 +257,26 @@ class ContainerManager(object):
                         + ' Check if /var/run/docker.sock exists.' )
                     self._logger.error(e)
 
-                    exit(-1)
+                    os.exit(-1)
 
                 except requests.exceptions.HTTPError as e:
                     self._logger.debug('Bad answer from server.')
                     self._logger.error(e)
 
-                    exit(-1)
+                    os.exit(-1)
 
         except ValueError as e:
             self._logger.debug('JSON ValueError: parse error. Please check the ' \
                 + 'consistency of your %s file.' % self._container_conf_file)
             self._logger.error(e)
 
-            exit(-1)
+            os.exit(-1)
 
         except IOError as e:
             self._logger.debug('IOError: %s file not found.' % self._container_conf_file)
             self._logger.error(e)
 
-            exit(-1)
+            os.exit(-1)
 
     ## Start a created container.
     #
@@ -234,7 +295,7 @@ class ContainerManager(object):
             self._logger.debug('HTTPError: ' + request.content)
             self._logger.error(e)
 
-            exit(-1)
+            os.exit(-1)
 
     ## Clean up the container list.
     #
@@ -261,38 +322,46 @@ class ContainerManager(object):
                     if id in self._container_registry:
                         try:
                             request = self._session.delete('http+unix://' \
-                                + self._socket_path + '/containers/' + listjson[i]['Id'])
+                                + self._socket_path + '/containers/' + id)
                             request.raise_for_status()
 
                             self._logger.info('Deleted: %s since its status was: %s' \
                                 % ( listjson[i]['Id'][0:12], listjson[i]['Status']))
+
+                            tmp_reg = self._container_registry
+                            tmp_reg = [x for x in tmp_reg if x != id]
+                            self._container_registry = tmp_reg
+
+                            self._registry_file_update()
+                            
                             ## Synchronize the backup file.
-                            tmp_list = []
-                            with open(self._container_registry_file, 'r') as registry:
-                                tmp_list = registry.readlines()
-                            with open(self._container_registry_file, 'w') as registry:
-                                for line in tmp_list:
-                                    if line != id:
-                                        registry.write(line)
+                            # tmp_list = []
+                            # with open(self._container_registry_file, 'r') as registry:
+                            #     tmp_list = registry.readlines()
+                            # with open(self._container_registry_file, 'w') as registry:
+                            #     for line in tmp_list:
+                            #         if line != id:
+                            #             registry.write(line)
 
                         except requests.exceptions.HTTPError as e:
                             self._logger.debug('HTTPError: ' + request.content)
                             self._logger.error(e)
 
-                            exit(-1)
+                            os.exit(-1)
 
         except requests.exceptions.ConnectionError:
             self._logger.error('Connection Err: can\'t find a proper socket.' \
                 + ' Is the docker daemon running correctly?' \
                 + ' Check if /var/run/docker.sock exists.' )
 
-            exit(-1)
+            os.exit(-1)
 
         except requests.exceptions.HTTPError as e:
             self._logger.debug('Bad answer from server.')
             self._logger.error(e)
 
-            exit(-1)
+            os.exit(-1)
+
 
     def _container_respawner(self):
         pass
@@ -302,8 +371,8 @@ class ContainerManager(object):
 
 
 manager = ContainerManager()
-manager._setup_logfile()
-manager._load_conf('conf/manager.conf')
+manager._setup_logger()
+manager._setup_from_conf('conf/manager.conf')
 manager._list_container()
 ID = manager._build_container()
 manager._start_container(ID)
