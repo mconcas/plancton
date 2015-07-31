f799d9bef0cd2d2102a1b3ac2be15d6396042012
Matteo Concas <mett1990@gmail.com>
2015-07-31 14:14:04 +0200

    Fixed issue #1

--- container-spawner.py
+++ container-spawner.py
@@ -1,39 +1,35 @@
 #!/bin/python
 # -*- coding: utf-8 -*-
 
-import os, sys
+import os, sys, time
 import errno
 import json
-import time
 import logging, logging.handlers
-
-from stat import *
-from configobj import ConfigObj
-
-try:
-    import requests_unixsocket, requests.exceptions
-except ImportError, e:
-    print 'Couldn\'t find the requests_unixsocket module. Please check the prereqs...' \
-        + str(e)
-
-    sys.exit(-1)
+import requests_unixsocket, requests, requests.exceptions as reqexc
+import base64
+# from configobj import ConfigObj
 
 class ContainerPoolManager(object):
     __version__ = '0.0.2'
 
-    def __init__(self):
-        self._name = 'ContainerManager'
+    def __init__(self, logpath='/tmp/container-pool-manager.log'):
+        self._name = 'ContainerPoolManager'
         self._logger = logging.getLogger(self._name)
 
         ## Can be overridden by manual configuration.
-        self._logpath = '/tmp/container-manager.log'
-        self._session = requests_unixsocket.Session()
-        # self._container_registry = []
+        self._logpath = logpath
 
-    ## _setup_logger: sets up logs.
+        ## *requests sessions
+        self._unix_session = requests_unixsocket.Session()
+        self._https_session = requests.Session()
+
+        ## JSON container configuration
+        self._json_cconfig = []
+
+    ## _logger_setup: sets up logs.
     #
     # @return nothing
-    def _setup_logger(self):
+    def _logger_setup(self):
         format = '%(asctime)s %(name)s %(levelname)s ' \
             + '[%(module)s.%(funcName)s] %(message)s'
         datefmt = '%Y-%m-%d %H:%M:%S'
@@ -47,138 +43,132 @@ class ContainerPoolManager(object):
     ## _setup_from_conf: sets up from configfile.
     #
     # @return nothing
-    def _setup_from_conf(self, file_path):
-        ## This function is called ONCE at the startup, it loads configurations and
-        #  try to restore the container list from a backup file.
+    def _initialize(self):
+        '''
+        Function usually called at the startup.
+        It downloads the configurations via HTTPS from a specified URL.
+        '''
 
-        self._logger.info('Loading cfg from: ' + file_path)
-        self._config = ConfigObj(file_path)
-        mancfg = 'Manager Configuration'
-        logpath = self._config[mancfg]['LogPath']
-        if not logpath == self._logpath:
-            self._logger.info('Overriding default conf. setting logfile to:' + logpath)
-        self._logpath = logpath
-        self._socket_path = self._config[mancfg]['SocketPath']
-        self._container_conf_file = self._config[mancfg]['ContainerConf']
+        try:
+            self._logger.info('Downloading container configuration from GitHub...')
 
-    ## _list_container: lists existing containers and their own statuses.
-    #
-    #  NEW:
-    #  Now it also performs an ID check with the internally stored list, substantially
-    #  making container-manager blind to "not owned" containers.
-    #  Thus is provided a sort of isolation.
+            ## Change this url to load custom configuration.
+            #  ref=string The name of the commit/branch/tag.
+            #  Default: the repository’s default branch (usually master)
+            #  (https://developer.github.com/v3/repos/contents/)
+            httpsrequest = self._https_session.get('https://api.github.com/repos/'
+                + 'mconcas/plancton/contents/conf/worker_centos6.json?ref=master')
+            httpsrequest.raise_for_status()
+
+            encodedcfg = json.loads(httpsrequest.content)['content']
+            if encodedcfg:
+                self._logger.debug('Found cfg 64-based string...')
+                self._json_cconfig = json.loads(base64.b64decode(encodedcfg))
+            else:
+                self._logger.warning('Empty cfg string, skipping conf initialization...')
+
+        except Exception as e:
+            self._logger.error(e)
+
+        ## Docker unix-socket path, usually located in /var/run
+        #  It likely won't be moved.
+        self._socket_path = '%2Fvar%2Frun%2Fdocker.sock'
+
+    ## _list_containers: lists existing containers and their own statuses.
+    # 
+    #  List the running containers having a specified label, that is stored by
+    #  docker during the build phase.
     #
     #  @return the list length.
-    def _list_container(self, quiet=False):
+    def _list_containers(self, taglabel='worker-node', quiet=False):
         """
-        This function wraps some requests, prints container list in the log file
-        and returns the number of running _owned_ containers.
+        Function that wraps some requests, prints container list to a log file
+        and returns the number of running containers found with a specific tag.
         """
+
         try:
             self._logger.info('Checking container list...')
-            request = self._session.get('http+unix://' + self._socket_path
+            unixrequest = self._unix_session.get('http+unix://' + self._socket_path
                 + '/containers/json?all=1')
-            request.raise_for_status()
-            self._jsoncontlist = json.loads(request.content)
+            unixrequest.raise_for_status()
+            self._jscontlist = json.loads(unixrequest.content)
 
-            self._contnum = len(self._jsoncontlist) # Total containers number.
+            self._contnum = len(self._jscontlist) # Total containers number.
             self._logger.debug('Found %r container(s) in the whole Docker database.' \
                 % self._contnum)
-            self._logger.info('Listing below...')
 
-            ## Since the APIs return all the containers in the list, even the ones ran
-            #  by other users, we must provide, at some point, a kind of isolation.
-            #  NEW: isolation is now provided by read a special label: container.label
-            #  and asking it corresponds to a specified value.
-
-            owned_containers = 0
-
+            self._owned_containers = 0
+            if not quiet:
+                self._logger.info('Listing below...')
             for i in range(0, self._contnum):
                 try:
-                    contlabel = self._jsoncontlist[i]['Labels']['container.label']
-                    if contlabel == 'worker-node':
+                    contlabel = self._jscontlist[i]['Labels']['container.label']
+                    if self._jscontlist[i]['Labels']['container.label'] == taglabel:
                         owner = 'spawner-agent'
-                        owned_containers+=1
+                        self._owned_containers+=1
                     else:
                         owner = 'others       '
 
+                ## It's likely that user-runned containers do not have the tag label...
                 except KeyError:
                     owner = 'others       '
 
-                status = self._jsoncontlist[i]['Status']
+                status = self._jscontlist[i]['Status']
                 if not status:
                     status = 'Created (Not Running)'
                 if not quiet:
                     self._logger.info('\t [ ID: %s ] [ OWNER: %s ] [ STATUS: %s ] ' \
-                        % (id, owner, status))
+                        % (self._jscontlist[i]['Id'], owner, status))
 
-            self._logger.info('Found %r "owned" container(s).' % owned_containers)
+            self._logger.info('Found %r owned container(s).' % self._owned_containers)
 
-            return owned_containers
+            return self._owned_containers
 
-        except requests.exceptions.ConnectionError:
+        except reqexc.ConnectionError:
             self._logger.debug('Connection Error: couldn\'t find a proper socket. '
                 + ' Is the Docker daemon running correctly?'
                 + ' Check if /var/run/docker.sock exists.' )
 
-            sys.exit(-1)
-
-        except requests.exceptions.HTTPError as e:
+        except reqexc.HTTPError as e:
             print 'HTTPError raised, please check ' + self._logpath + \
             ' for further informations.'
-            self._logger.debug('Bad answer from server. \n')
+            self._logger.debug('Bad response from server. \n')
             self._logger.error(e)
-
-            sys.exit(-1)
 
     ## Create a container following from a json cfg file.
     #
     # @return nothing.
     def _build_container(self):
         """
-        This function creates a container. It returns, if success, the container id.
-        Register the built container into a local registry (this is because
-        isolation MUST be provided).
+        This function creates a container. It returns, in case of success, the id.
         """
+        headers = {'content-type':'application/json', 'Accept':'text/plain'}
         try:
-            with open(self._container_conf_file) as data_file:
-                data = json.load(data_file)
-                headers = {'content-type':'application/json', 'Accept':'text/plain'}
-                try:
-                    request = self._session.post('http+unix://' + self._socket_path \
-                        + '/containers/create', data=json.dumps(data), headers=headers)
-                    request.raise_for_status()
-                    self._logger.debug('Successfully created: ' + str(request.content))
-                    c_id = str(json.loads(request.content)['Id'])
+            unixrequest = self._unix_session.post('http+unix://' \
+                + self._socket_path + '/containers/create',      \
+                data=json.dumps(self._json_cconfig), headers=headers)
 
-                    return c_id
+            unixrequest.raise_for_status()
+            self._logger.debug('Successfully created: '          \
+                + str(unixrequest.content))
 
-                except requests.exceptions.ConnectionError as e:
-                    self._logger.debug('Conn. Error: couldn\'t find a proper socket.'
-                        + ' Is the Docker daemon running correctly?'
-                        + ' Check if /var/run/docker.sock exists.' )
-                    self._logger.error(e)
+            c_id = str(json.loads(unixrequest.content)['Id'])
 
-                    sys.exit(-1)
+            return c_id
 
-                except requests.exceptions.HTTPError as e:
-                    self._logger.debug('Bad answer from server.')
-                    self._logger.error(e)
+        except reqexc.ConnectionError as e:
+            self._logger.debug('Conn. Error: couldn\'t find a proper socket.'
+                + ' Is the Docker daemon running correctly?'
+                + ' Check if /var/run/docker.sock exists.' )
+            self._logger.error(e)
 
-                    sys.exit(-1)
+        except reqexc.HTTPError as e:
+            self._logger.debug('Bad response from server.')
+            self._logger.error(e)
 
         except ValueError as e:
-            self._logger.debug('JSON ValueError: parse error. Please check the ' \
-                + 'consistency of your %s file.' % self._container_conf_file)
+            self._logger.debug('JSON ValueError: parser error.')
             self._logger.error(e)
-
-            sys.exit(-1)
-
-        except IOError as e:
-            self._logger.debug('IOError: %s file not found.' % self._container_conf_file)
-            self._logger.error(e)
-
-            sys.exit(-1)
 
     ## Start a created container.
     #
@@ -188,59 +178,54 @@ class ContainerPoolManager(object):
         This function wraps some requests and starts a container by getting its ID.
         """
         try:
-            request = self._session.post('http+unix://' + self._socket_path \
+            unixrequest = self._unix_session.post('http+unix://' + self._socket_path \
                 + '/containers/' + id + '/start')
-            request.raise_for_status()
-            self._list_container()
+            unixrequest.raise_for_status()
+            self._list_containers()
 
-        except requests.exceptions.HTTPError as e:
-            self._logger.debug('HTTPError: ' + request.content)
+        except reqexc.HTTPError as e:
+            self._logger.debug('HTTPError: ' + unixrequest.content)
             self._logger.error(e)
-
-            sys.exit(-1)
 
     ## Garbage collector.
     #
-    # @return nothing.
-    def _container_reaper(self):
+    #  @return nothing.
+    def _container_cleaner(self):
         """
         Wipes out all the Created and Exited containers.
         """
 
-        self._list_container(True) # Update jsonlist
-        # for id in self._container_registry:
-        for i in range(0, len(self._jsoncontlist)):
-            status = self._jsoncontlist[i]['Status']
+        self._list_containers(True) # Update self._jscontlist
+        for i in range(0, len(self._jscontlist)):
+            status = self._jscontlist[i]['Status']
             if not status or 'Exited' in status:
-                id = self._jsoncontlist[i]['Id']
                 try:
-                    contlabel = self._jsoncontlist[i]['Labels']['container.label']
+                    contlabel = self._jscontlist[i]['Labels']['container.label']
                     if contlabel == 'worker-node':
                         ## Exited and owned.
+                        id = self._jscontlist[i]['Id']
                         try:
-                            request = self._session.delete('http+unix://' \
+                            unixrequest = self._unix_session.delete('http+unix://' \
                                 + self._socket_path + '/containers/' + id)
-                            request.raise_for_status()
-                        except requests.exceptions.HTTPError as e:
-                            self._logger.debug('HTTPError: ' + request.content)
+                            unixrequest.raise_for_status()
+                            self._logger.debug('Removed ' + id + ' successfully')
+                        except reqexc.HTTPError as e:
+                            self._logger.debug('HTTPError: ' + unixrequest.content)
                             self._logger.error(e)
-                            self._logger.warning('It couldn\'t be possibile to delete '
-                                + ' container: ' + id)
+                            self._logger.warning('Couldn\'t delete: ' + id)
                 except:
                     pass
 
-#=========================================================================================
-def container_respawner(cont_num):
+#=========================== dummy function ==============================================
+def dummy_container_respawner(cont_num):
     manager = ContainerPoolManager()
-    manager._setup_logger()
-    manager._setup_from_conf('conf/manager.conf')
+    manager._logger_setup()
+    manager._initialize()
     while(1):
-        owning = manager._list_container()
-
+        owning = manager._list_containers()
         if (owning < cont_num):
             ID = manager._build_container()
             manager._start_container(ID)
+        manager._container_cleaner()
 
-        manager._container_reaper()
-
-container_respawner(3)
+dummy_container_respawner(3)
