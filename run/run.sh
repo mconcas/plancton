e22bd354d78bf8bc6588817ddfbb46dd60e8f440
Matteo Concas <mconcas@cern.ch>
2015-09-15 21:45:44 +0200

    Update run.sh

--- run/run.sh
+++ run/run.sh
@@ -6,11 +6,7 @@ echo "|Runscript called.|"
 echo "-------------------"
 [[ ! -e $DEST/.git ]] && git clone https://github.com/mconcas/plancton $DEST
 cd $DEST
-git clean -f -d
-git clean -fX -d
-git remote update -p
-git fetch
-git fetch --tags
+
 git reset --hard origin/master
 
 export PATH=$PATH:$DEST/bin
