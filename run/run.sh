29f36e1778e11b2f0a23c21e962344b5bef7f37b
Matteo Concas <mconcas@cern.ch>
2015-09-15 22:06:40 +0200

    Update run.sh

--- run/run.sh
+++ run/run.sh
@@ -4,12 +4,13 @@ DEST=/opt/plancton/git
 echo "-------------------"
 echo "|Runscript called.|"
 echo "-------------------"
-[[ ! -e $DEST/.git ]] && git clone https://github.com/mconcas/plancton $DEST
-cd $DEST
+function main() {
+    [[ ! -e $DEST/.git ]] && git clone https://github.com/mconcas/plancton $DEST
+    cd $DEST
+    git reset --hard origin/master
+    export PATH=$PATH:$DEST/bin
+    export PYTHONPATH=$PYTHONPATH:$DEST/pylibs
+    planctonctl start
+}
 
-git reset --hard origin/master
-
-export PATH=$PATH:$DEST/bin
-export PYTHONPATH=$PYTHONPATH:$DEST/pylibs
-
-planctonctl start
+main
