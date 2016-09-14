e54260357302d18fb443640daf04f0f1194d0914
Matteo Concas <mconcas@cern.ch>
2016-09-14 15:51:51 +0200

    Fix crontab task (#58)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
   name='plancton',
-  version='0.4.3',
+  version='0.4.5',
   description='An opportunistic distributed computing project based on Docker',
   long_description=long_description,
   url='https://github.com/mconcas/plancton',
