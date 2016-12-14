916da356c349a067aff04275ac1b58c77603bb11
Matteo Concas <mconcas@cern.ch>
2016-12-14 18:20:10 +0100

    Add CapDrop generator (#114)
    * Add CapDrop option to plancton configuration

--- plancton/__init__.py
+++ plancton/__init__.py
@@ -270,7 +270,8 @@ class Plancton(Daemon):
                            "Devices"     : [ dict(zip([ "PathOnHost", "PathInContainer",
                                                         "CgroupPermissions" ], x.split(":", 2)))
                                              for x in self.conf["devices"] ],
-                           "CapAdd"      : self.conf["capabilities"]
+                           "CapAdd"      : [ x.lstrip("+") for x in self.conf["capabilities"] if x and x[0]!="-" ],
+                           "CapDrop"     : [ x.lstrip("-") for x in self.conf["capabilities"] if x and x[0]=="-" ]
                          }
         }
     self.logctl.debug("Container definition for %s:\n%s" % (cname, json.dumps(c, indent=2)))
